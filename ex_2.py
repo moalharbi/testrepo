7c63979999c285ff7bf96718e665ad14c7a0f112
Mohammed ALHARBI <11207601+moalharbi@users.noreply.github.com>
2024-01-31 20:21:26 +0300

    Add files via upload

---
+++ ex_2.py
@@ -0,0 +1,13 @@
+"""
+Exercise Question 2: Print the following pattern
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+"""
+for i in range(5):
+    for j in range(i+1):
+        print(j+1, end=' ')
+    print('\n')
+        
